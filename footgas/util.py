09f70a327d570a9c1d5933ea601005f06c47d27a
Markus Wang Halvorsen <markus@halvorsenfamilien.com>
2023-03-15 01:25:12 +0100

    better string-time conversions

--- footgas/util.py
+++ footgas/util.py
@@ -1,38 +1,29 @@
+from datetime import timedelta
+
+
 def ftime(time: int, add_ms: bool = True) -> str:
     '''
     Turns ms time into a formatted time string
     '''
-    s = time * 1e-3
-    ms = int((s - int(s)) * 1e3)
-    min = int(s // 60)
+    time = timedelta(milliseconds=time)
 
-    formatted = f'{str(min).rjust(2, "0")}:{str(int(s) % 60).rjust(2, "0")}'
+    minutes = str(time.seconds // 60).rjust(2, '0')
+    seconds = str(int(time.seconds % 60)).rjust(2, '0')
     if add_ms:
-        formatted += f'.{str(ms).rjust(3, "0")}'
-    return formatted
+        seconds += '.' + str(time.microseconds)[:-3].ljust(3, '0')
+    return f'{minutes}:{seconds}'
 
 
-def strtoms(time) -> int:
+def strtoms(time: str) -> int:
     '''
     Turns a string into its corresponding millisecond timestamp
     '''
-    m, s, ms = 0, 0, 0
     if not time or time.isalpha():
         return None
 
-    # validate time formatting
-    time = time.split(':')
-    if not time[0]:
-        return None
-    if len(time) < 2 or time[0].isalpha() or time[1].isalpha():
-        return None
-    m = int(time[0])
-
-    s_ms = time[1].split('.')
-    if not s_ms[0] or s_ms[0].isalpha():
-        return None
-    s = int(s_ms[0])
-    if len(s_ms) > 1 and not s_ms[1].isalpha():
-        ms = int(s_ms[1])
-
-    return int(((m * 60) + s) * 1e3 + ms)
+    times = list(filter(lambda t: t, time.split(':')))
+    kwargs = zip(('minutes', 'seconds'), times)
+    delta = timedelta(**{
+        key: float(val) for key, val in kwargs
+    })
+    return delta.seconds * 1e3 + delta.microseconds / 1e3
